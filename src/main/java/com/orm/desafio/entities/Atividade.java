dcfa7f416fdaac86418b954029746172e0ae822e
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2024-08-28 15:36:16 -0300

    Update Atividade.java

--- src/main/java/com/orm/desafio/entities/Atividade.java
+++ src/main/java/com/orm/desafio/entities/Atividade.java
@@ -1,63 +1,56 @@
-package com.orm.desafio.entities;
-
+package com.desafio.modeloDominioEORM.entities;
+import jakarta.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Objects;
+import java.util.List;
 import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_atividade")
+@Table(name="tb_atividade")
 public class Atividade {
+
+  
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
+    private Integer id;
     private String nome;
+    @Column(columnDefinition = "TEXT")
     private String descricao;
     private Double preco;
 
+    
     @ManyToOne
-    @JoinColumn(name = "categoria_id")  // Nome da coluna de chave estrangeira na tabela 'tb_atividade'
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
-    @OneToMany(mappedBy = "atividade")
-    private Set<Bloco> blocos = new HashSet<>();
-
     @ManyToMany
-    @JoinTable(
-        name = "atividade_participante",
-        joinColumns = @JoinColumn(name = "atividade_id"),
-        inverseJoinColumns = @JoinColumn(name = "participante_id")
-    )
+    @JoinTable(name = "tb_atividade_participante",
+                    joinColumns = @JoinColumn(name = "atividade_id"),
+                    inverseJoinColumns = @JoinColumn(name = "participante_id"))
     private Set<Participante> participantes = new HashSet<>();
 
-    // Construtores
-    public Atividade() {}
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
 
-    public Atividade(Long id, String nome, String descricao, Double preco, Categoria categoria) {
+    //constructors
+    public Atividade() {
+
+    }
+
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.preco = preco;
-        this.categoria = categoria;
     }
 
-    // Getters e Setters
-    public Long getId() {
+    
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -93,32 +86,13 @@ public class Atividade {
         this.categoria = categoria;
     }
 
-    public Set<Bloco> getBlocos() {
-        return blocos;
-    }
-
-    public void setBlocos(Set<Bloco> blocos) {
-        this.blocos = blocos;
-    }
-
+    
     public Set<Participante> getParticipantes() {
         return participantes;
     }
 
-    public void setParticipantes(Set<Participante> participantes) {
-        this.participantes = participantes;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Atividade other = (Atividade) obj;
-        return Objects.equals(id, other.id);
+   
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
