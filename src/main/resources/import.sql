ad605ef836844b2768b5d38158138bcd2788a7ae
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2024-08-28 15:35:19 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,47 +1,20 @@
-INSERT INTO tb_categoria(nome) VALUES ('Curso');
-INSERT INTO tb_categoria(nome) VALUES ('Oficina');
+INSERT INTO tb_categoria(descricao) VALUES ('Curso');
+INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
 
-INSERT INTO tb_participante(email,nome) VALUES ('jose@gmail.com','José Silva');
-INSERT INTO tb_participante(email,nome) VALUES ('tiago@gmail.com','Tiago Faria');
-INSERT INTO tb_participante(email,nome) VALUES ('maria@gmail.com','Maria do Rosário');
-INSERT INTO tb_participante(email,nome) VALUES ('teresa@gmail.com','Teresa Silva');
+INSERT INTO tb_participante(nome, email) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
 
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Curso de HTML',  'Aprenda HTML de forma prática', 80.00, 1);
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.00, 2);
 
-INSERT INTO tb_atividade(nome,descricao,preco,categoria_id) VALUES ('Curso de HTML','Aprenda HTML de forma prática',80.00,1);
-INSERT INTO tb_atividade(nome,descricao,preco,categoria_id) VALUES ('Oficina de GitHub','Controle versões de seus projetos',50.00,2);
-
-INSERT INTO atividade_participante(atividade_id,participante_id) VALUES (1,1);
-INSERT INTO atividade_participante(atividade_id,participante_id) VALUES (1,2);
-INSERT INTO atividade_participante(atividade_id,participante_id) VALUES (1,3);
-INSERT INTO atividade_participante(atividade_id,participante_id) VALUES (2,3);
-INSERT INTO atividade_participante(atividade_id,participante_id) VALUES (2,4);
-
-INSERT INTO tb_bloco (atividade_id, inicio, fim) VALUES (1, '2017-09-25 08:00:00', '2017-09-25 11:00:00');
-INSERT INTO tb_bloco (atividade_id, inicio, fim) VALUES (2, '2017-09-25 14:00:00', '2017-09-25 18:00:00');
-INSERT INTO tb_bloco (atividade_id, inicio, fim) VALUES (3, '2017-09-26 08:00:00', '2017-09-26 11:00:00');
-
-
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (1,1);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (1,2);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (1,3);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (2,3);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (2,4);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (3,3);
-
-INSERT INTO participante_bloco(bloco_id,participante_id) VALUES (3,4);
-
-
-
-
-
-
-
-
-
-
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 1);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 2);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 3);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 1);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 3);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 4);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00', '2017-09-25T11:00:00', 1);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00', '2017-09-25T18:00:00', 2);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00', '2017-09-26T11:00:00', 2);
